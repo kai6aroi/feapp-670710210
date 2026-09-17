7d9edd8d244acd754465b463dfe3e8ca7af069b9
kai6aroi <romejoomjang_p@silpakorn.edu>
2026-09-17 03:04:21 +0000

    ready to deploy

--- pooh-app/src/App.js
+++ pooh-app/src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MovieList from './components/MovieList';
+
 import Card from "./components/Card";
-import Greeting from "./Greeting";
-const students = [
-  { id: 1, name: 'ฝน',   year: 3 },
-  { id: 2, name: 'เต้ย', year: 2 },
-  { id: 3, name: 'มายด์', year: 4 },
-];
 
 function App() {
   return (
